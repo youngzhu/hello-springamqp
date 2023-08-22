28b20204e3fe385899d60a7d6b41090db4afd3d1
young-win <young.zhu.2012@gmail.com>
2023-08-22 15:23:45 +0800

    v0.2.0 WorkQueue

--- consumer/src/main/java/com/youngzy/amqp/listener/SpringRabbitListener.java
+++ consumer/src/main/java/com/youngzy/amqp/listener/SpringRabbitListener.java
@@ -1,7 +1,11 @@
 package com.youngzy.amqp.listener;
 
+import lombok.SneakyThrows;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class SpringRabbitListener {
@@ -10,4 +14,20 @@ public class SpringRabbitListener {
      public void listenSimpleQueue(String msg) {
          System.out.println("消费者接收到的消息：【" + msg + "】");
      }
+
+    @SneakyThrows
+    @RabbitListener(queues = "queue.work")
+    public void listenWorkQueue1(String msg) {
+        System.out.println("消费者【1】接收到的消息：【" + msg + "】" + LocalTime.now());
+        TimeUnit.MILLISECONDS.sleep(20);
+    }
+
+    @SneakyThrows
+    @RabbitListener(queues = "queue.work")
+    public void listenWorkQueue2(String msg) {
+         // 为了明显区分
+        System.err.println("====消费者【2】接收到的消息：【" + msg + "】" + LocalTime.now());
+//        TimeUnit.MILLISECONDS.sleep(200);
+        Thread.sleep(100);
+    }
 }
--- publisher/src/test/java/com/youngzy/amqp/spring/SpringAmqpTest.java
+++ publisher/src/test/java/com/youngzy/amqp/spring/SpringAmqpTest.java
@@ -1,11 +1,15 @@
 package com.youngzy.amqp.spring;
 
+import lombok.SneakyThrows;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalTime;
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,6 +24,17 @@ public class SpringAmqpTest {
         String queueName = "queue.simple";
         String message = "Hello, Spring AMQP!";
         rabbitTemplate.convertAndSend(queueName, message);
+    }
+
+    @SneakyThrows
+    @Test
+    public void testSendMessage2WorkQueue() {
+        String queueName = "queue.work";
+
+        for (int i = 1; i <= 50; i++) {
+            rabbitTemplate.convertAndSend(queueName, "第【" + i + "】次问候！！" + LocalTime.now());
+            TimeUnit.MILLISECONDS.sleep(10);
+        }
     }
 
 }
